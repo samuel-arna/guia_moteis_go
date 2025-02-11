b6866e8618f851444778b75a80ce703f8f7b0598
Samuel Arná <samuka.arna@gmail.com>
2025-02-11 00:33:43 -0300

    fix: remove unnecessary code

--- lib/core/config/http_config.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-class HttpConfig {
-  Future<HttpClientRequest> get(String url) async {
-    HttpClient client = HttpClient();
-    return client.getUrl(Uri.parse(url));
-  }
-}
